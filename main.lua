c09bf387cd9a94f7513aec5215b9352711c7e151
WillBurbidge <wb734@york.ac.uk>
2024-06-19 19:46:57 +0100

    move the text

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- This is a test ... WIll had just added this hello, hi!
-
+pixel = 600
 
 success = love.window.setMode( 1400, 800 )
 
@@ -9,6 +9,15 @@ function love.load()
     love.graphics.setBackgroundColor(255,155,255)
  end
 
+ function love.update(dt)
+    if love.keyboard.isDown("up") then
+       pixel = pixel + 1
+    end
+    if love.keyboard.isDown("down") then
+        pixel = pixel - 1
+     end
+ end
+
 function love.draw()
-    love.graphics.print("Test", 800, 600)
+    love.graphics.print("Test", 800, pixel)
 end
