2943d3e9ec2f6c0b855d3e8cef2a8ad67053c4e7
WillBurbidge <wb734@york.ac.uk>
2024-06-19 19:42:54 +0100

    test

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 -- This is a test ... WIll had just added this hello, hi!
+
+
 success = love.window.setMode( 1400, 800 )
+
+function love.load()
+    love.graphics.setNewFont(12)
+    love.graphics.setColor(0,40,0)
+    love.graphics.setBackgroundColor(255,155,255)
+ end
+
 function love.draw()
     love.graphics.print("Test", 800, 600)
 end
